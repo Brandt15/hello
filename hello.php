7847e3aa1f2e057630be313cf3140fda1ea7d010
Kristina Brandt <brandt.stina@gmail.com>
2015-09-26 12:58:39 +0200

    Major change

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 	<title>Hello World</title>
 </head>
 <body>
-	<h1>Hello, new version is upcoming</h1>
-	<?php echo "My name is Mega S"; ?>
+	<h1>Hello, new version is upcoming. Version 3</h1>
+	<?php echo "My name is Mega Stina"; ?>
 </body>
-</html>>
+</html>
